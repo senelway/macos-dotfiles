91e4ba937b9ebff4b6a8d168c88f75e18e471596
senelway <senelway@gmail.com>
2023-03-23 22:14:30 +0700

    :mute: remove trash keybinding

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,9 +5,6 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["|"] = {":vsplit <cr>"},
-    ["ff"] = {":Telescope fd <cr>"},
-    ["fg"] = {":Telescope live_grep <cr>"},
-    ["fs"] = {":Telescope git_status <cr>"},
   },
   i = {
      ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
