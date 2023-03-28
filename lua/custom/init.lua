85881a6afa5c1aad768651260d4ac777e74d0f92
senelway <senelway@gmail.com>
2023-03-28 13:28:22 +0700

    :alien: upd

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -25,22 +25,27 @@ autocmd("BufWritePre", {
 
 autocmd("BufEnter", {
   pattern = "*",
-  command = "set title",
+  command = "set title | set relativenumber",
 })
 
 vim.api.nvim_create_user_command("GHOpen", function()
   local remote = vim.fn.system "git ls-remote --get-url origin"
-  if string.match(remote, ".*[github|gitlab].*") == false then
+  -- local branch = vim.fn.system "git rev-parse --abbrev-ref HEAD"
+
+  if string.match(remote, ".*[github].*") == false then
     print "Not a github or gitlab repo"
     return
   end
 
   if string.match(remote, "^git") then
+    remote = string.gsub(remote, "[\n\r]", "")
     remote = string.gsub(remote, ":", "/")
     remote = string.gsub(remote, "^git@", "https://")
+    remote = string.gsub(remote, ".git$", "")
   end
 
-  local url = string.format('open "%s"', remote)
+  local path = string.format("%s/three/%s", remote)
+  local url = string.format('open "%s"', path)
 
   os.execute(url)
 end, { nargs = 0 })
