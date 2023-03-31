767dbfe06738a6f542ebb7276222ab40a76bb511
senelway <senelway@gmail.com>
2023-03-31 09:36:52 +0700

    :bookmark: bootstrap

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -1,66 +1,64 @@
 local M = {}
 
+M.echo = function(str)
+  vim.cmd "redraw"
+  vim.api.nvim_echo({ { str, "Bold" } }, true, {})
+end
+
 M.lazy = function(install_path)
-  print "Bootstrapping lazy.nvim .."
+  ------------- base46 ---------------
+  local lazy_path = vim.fn.stdpath "data" .. "/lazy/base46"
 
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    install_path,
-  }
+  M.echo "  Compiling base46 theme to bytecode ..."
 
+  local base46_repo = "https://github.com/NvChad/base46"
+  vim.fn.system { "git", "clone", "--depth", "1", "-b", "v2.0", base46_repo, lazy_path }
+  vim.opt.rtp:prepend(lazy_path)
+
+  require("base46").compile()
+
+  --------- lazy.nvim ---------------
+  M.echo "  Installing lazy.nvim & plugins ..."
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", repo, install_path }
   vim.opt.rtp:prepend(install_path)
 
-  -- install plugins + compile their configs
+  -- install plugins
   require "plugins"
-
   vim.api.nvim_buf_delete(0, { force = true }) -- close lazy window
 
-  vim.defer_fn(function()
-    vim.cmd "silent! MasonInstallAll"
-  end, 0)
+  ---------- mason packages -------------
+  vim.schedule(function()
+    vim.cmd "MasonInstallAll"
+    local packages = table.concat(vim.g.mason_binaries_list, " ")
+
+    require("mason-registry"):on("package:install:success", function(pkg)
+      packages = string.gsub(packages, pkg.name:gsub("%-", "%%-"), "") -- rm package name
+
+      if packages:match "%S" == nil then
+        vim.schedule(function()
+          vim.api.nvim_buf_delete(0, { force = true })
+          M.echo "Now please read the docs at nvchad.com!! 󰕹 󰱬"
+        end)
+      end
+    end)
+  end)
 end
 
 M.gen_chadrc_template = function()
   if not vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1] then
-    local input = vim.fn.input "Do you want to install chadrc template? (y/n) : "
-    vim.cmd "redraw|echo ''"
+    local path = vim.fn.stdpath "config" .. "/lua/custom/"
+    local input = vim.fn.input "Do you want to install example custom config? (y/N) : "
 
     if input == "y" then
-      print "cloning chadrc starter template repo...."
-
-      vim.fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/NvChad/example_config",
-        vim.fn.stdpath "config" .. "/lua/custom",
-      }
-      print "dont forget to read docs from nvchad.com!"
-      vim.cmd "redraw|echo ''"
-
-      -- delete .git from that repo
-      vim.loop.fs_rmdir(vim.fn.stdpath "config" .. "/lua/custom/.git")
-      vim.notify "successfully installed chadrc template!"
-      vim.cmd "redraw|echo ''"
+      M.echo "cloning example custom config repo ..."
+      vim.fn.system { "git", "clone", "--depth", "1", "https://github.com/NvChad/example_config", path }
+      vim.fn.delete(path .. ".git", "rf")
     else
-      local custom_dir = vim.fn.stdpath "config" .. "/lua/custom/"
-      vim.fn.mkdir(custom_dir, "p")
+      vim.fn.mkdir(path, "p")
 
-      local str = [[
-              local M = {}
-                M.ui = {
-                  theme = "onedark",
-               }
-              return M
-           ]]
-
-      local file = io.open(custom_dir .. "chadrc.lua", "w")
-      file:write(str)
+      local file = io.open(path .. "chadrc.lua", "w")
+      file:write "local M = {}\n M.ui = {theme = 'onedark'}\n return M"
       file:close()
     end
   end
