bfc423efd608557713cd5a5e7374d50a9f7e80d1
senelway <senelway@gmail.com>
2023-04-03 12:29:27 +0700

    :sparkles: introduce new default terminal

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -16,6 +16,11 @@ local browserOpen = function(link)
   os.execute(url)
 end
 
+-- autocmd("BufReadPre,FileReadPre", {
+--   pattern = "*",
+--   command = "NvimTreeToggle",
+-- })
+
 vim.api.nvim_create_user_command("GHOpen", function()
   local remote = vim.fn.system "git ls-remote --get-url origin"
   -- local branch = vim.fn.system "git rev-parse --abbrev-ref HEAD"
