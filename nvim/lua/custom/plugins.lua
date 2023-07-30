b45d5954637e4637ad3dabc19400a04aba838efa
senelway <senelway@gmail.com>
2023-07-30 18:48:35 +0700

    chore: readme update

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -4,7 +4,64 @@ local overrides = require "custom.configs.overrides"
 local plugins = {
   -- Override plugin definition options
   --
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
 
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -------------------------------
+  -------------------------------
+  -------------------------------
+  -------------------------------
+  -------------------------------
+  -------------------------------
+
+  {
+    "ahmedkhalf/project.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+    cmd = { "ProjectRoot" },
+    config = function()
+      require("project_nvim").setup {
+        manual_mode = true,
+      }
+    end,
+  },
   {
     "kdheepak/lazygit.nvim",
     dependencies = "nvim-lua/plenary.nvim",
@@ -78,52 +135,6 @@ local plugins = {
           show_url = true,
         },
       }
-    end,
-  },
-  -------------------------------
-  -------------------------------
-  -------------------------------
-  -------------------------------
-  -------------------------------
-  -------------------------------
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
-
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
-
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
     end,
   },
 }
