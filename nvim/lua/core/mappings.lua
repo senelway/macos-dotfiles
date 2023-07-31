46e5c985b371754691b9e570efcf8abc69f3a741
Dmitrii Strelkov <senelway@gmail.com>
2023-07-31 16:34:17 +0700

    feat: add move line hotkey (#36)
    - add `Alt+J|K` to alacrity config
    - add `<S-A-j>` and `<S-A-k>` hotkeys for moving line up/down

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -374,10 +374,7 @@ M.blankline = {
   n = {
     ["<leader>cc"] = {
       function()
-        local ok, start = require("indent_blankline.utils").get_current_context(
-          vim.g.indent_blankline_context_patterns,
-          vim.g.indent_blankline_use_treesitter_scope
-        )
+        local ok, start = require("indent_blankline.utils").get_current_context(vim.g.indent_blankline_context_patterns, vim.g.indent_blankline_use_treesitter_scope)
 
         if ok then
           vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = { "catppuccin", "monochrome" },
+  theme = "aquarium",
+  theme_toggle = { "aquarium", "catppuccin" },
   hl_override = highlights.override,
   hl_add = highlights.add,
   statusline = {
