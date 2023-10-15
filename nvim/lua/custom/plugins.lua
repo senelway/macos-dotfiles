4fc45b16b6e22b40e5402ad0d7f81a5f5cc6b0ed
senelway <senelway@gmail.com>
2023-10-15 14:40:52 +0400

    feat: null-ls replacemenet (wip)

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -9,7 +9,7 @@ local plugins = {
     dependencies = {
       -- format & linting
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         config = function()
           require "custom.configs.null-ls"
         end,
