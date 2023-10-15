d4a9707d8611c87c1492bc9a03cd3091b78aabef
senelway <senelway@gmail.com>
2023-10-16 00:07:43 +0400

    feat: fix hotkeys

--- nvim/lua/custom/plugins/term.lua
+++ nvim/lua/custom/plugins/term.lua
@@ -1,9 +1,9 @@
-vim.keymap.set('n', '<leader>i', ':ToggleTerm<CR>', { noremap = true, silent = true, desc = "Term toggle" })
-vim.keymap.set('n', '<leader>ti', ':ToggleTerm direction=horizontal<CR>', { noremap = true, silent = true, desc = "Term toggle" })
+vim.keymap.set('n', '<leader>i', ':ToggleTerm<CR>', { noremap = true, silent = true, desc = 'Term toggle' })
+vim.keymap.set('n', '<leader>ti', ':ToggleTerm direction=horizontal<CR>', { noremap = true, silent = true, desc = 'Term toggle' })
 
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
@@ -11,31 +11,29 @@ function _G.set_terminal_keymaps()
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
+vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
 
 function _lazygit_toggle()
-  local Terminal  = require('toggleterm.terminal').Terminal
-  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float', float_opts = { border = 'single', width = 270, height = 70 } })
+  local Terminal = require('toggleterm.terminal').Terminal
+  local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', float_opts = { border = 'single', width = 270, height = 70 } }
 
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>lg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
 
 return {
   'akinsho/toggleterm.nvim',
-  version = "*",
+  version = '*',
   config = function()
-    require("toggleterm").setup{
+    require('toggleterm').setup {
       direction = 'float',
       float_opts = {
         border = 'single',
         width = 200,
         height = 30,
         winblend = 3,
-      }
+      },
     }
   end,
 }
-
--- nvim/lua/custom/plugins/theme.lua
+++ nvim/lua/custom/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.cmd.colorscheme 'catppuccin'
+    require('catppuccin').setup {}
   end,
 }
