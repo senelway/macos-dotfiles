17b1334f2e7d32cf7a92e2af4da5492cb890ef52
senelway <senelway@gmail.com>
2023-10-15 23:44:09 +0400

    fix: lsp_fabllback

--- nvim/lua/custom/plugins/formatter.lua
+++ nvim/lua/custom/plugins/formatter.lua
@@ -5,7 +5,7 @@ return {
     return require('conform').setup {
       format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = false,
+        lsp_fallback = true,
       },
       formatters_by_ft = {
         lua = { 'stylua' },
