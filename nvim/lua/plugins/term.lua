657b547ad6c9b9c100ae6bba3012705f9dbe9b8e
senelway <senelway@gmail.com>
2023-10-18 20:21:35 +0400

    fix: change terminal

--- nvim/lua/plugins/term.lua
+++ nvim/lua/plugins/term.lua
@@ -1,43 +1,12 @@
-vim.keymap.set('n', '<leader>i', ':ToggleTerm<CR>', { noremap = true, silent = true, desc = '[I] Term toggle floating' })
-vim.keymap.set('n', '<leader>hi', ':ToggleTerm direction=horizontal<CR>',
-  { noremap = true, silent = true, desc = 'Term toggle [H]orizontal' })
-
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0, nowait = true }
-  vim.keymap.set('t', '<C-q>', [[<C-\><C-n><C-w>]], opts)
-end
-
-vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
-
-function _lazygit_toggle()
-  local Terminal = require('toggleterm.terminal').Terminal
-  local lazygit = Terminal:new {
-    cmd = 'lazygit',
-    hidden = true,
-    direction = 'float',
-    float_opts = {
-      border = 'single',
-      width = 270,
-      height = 65
-    }
-  }
-
-  lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap('n', '<leader>lg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>i', function() require("nvterm.terminal").toggle "float" end,
+  { desc = '[I] Term toggle floating' })
+vim.keymap.set('t', '<C-q>', vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true),
+  { desc = 'Exit edit mode in terminal', nowait = true })
+vim.keymap.set('n', '<leader>tt', ":terminal<CR>", { desc = '[T]erminal classic mode' })
 
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
+  "NvChad/nvterm",
   config = function()
-    require('toggleterm').setup {
-      direction = 'float',
-      float_opts = {
-        border = 'single',
-        width = 150,
-        height = 32,
-      },
-    }
+    require("nvterm").setup()
   end,
 }
