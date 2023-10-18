4774867ccee219bf6b5c2d8a1c5f962cd16690b2
senelway <senelway@gmail.com>
2023-10-18 12:55:05 +0400

    fix: set

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,14 +1,19 @@
 vim.o.backup = false
-
 vim.o.hlsearch = false
 vim.o.incsearch = true
-
-vim.wo.number = true
 vim.o.mouse = ''
-vim.o.clipboard = 'unnamedplus'
 vim.o.showmode = false
 vim.o.laststatus = 3
+vim.o.updatetime = 50
+vim.o.timeoutlen = 100
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
@@ -25,11 +30,6 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 50
-vim.opt.timeoutlen = 100
-vim.opt.completeopt:append("menuone,noselect")
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
+
+vim.wo.signcolumn = 'yes'
+vim.wo.number = true
