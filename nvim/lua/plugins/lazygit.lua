95991f5938548cc2b0221f108aa69b7a928450a8
senelway <senelway@gmail.com>
2024-01-13 17:42:38 +0000

    feat: remove lazygit

--- nvim/lua/plugins/lazygit.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set('n', '<leader>lg', function() require("lazygit").lazygit() end, { desc = '[L]azy [G]it' })
-
-return {
-  "kdheepak/lazygit.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-}
