bd53994fd570fc510b4b5a05c057fde2735d7178
senelway <senelway@gmail.com>
2024-01-14 20:18:42 +0000

    fix(tresitter): disable in vimdoc

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -17,7 +17,11 @@ return {
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
-  highlight = { enable = true },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+    disable = { 'vimdoc' }
+  },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
